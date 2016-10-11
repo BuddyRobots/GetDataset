55a563ca61d7d4da12eea8081671f4f46a1e537a
王茜 <wangqian@Wangqian.local>
2016-10-11 10:18:24 +0800

    update call OC_lua func

--- Assets/Scripts/GetImage.cs
+++ Assets/Scripts/GetImage.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using MagicCircuit;
 using System.Threading;
 using System.Runtime.InteropServices;
+using System;
 
 public class GetImage : MonoBehaviour
 {
@@ -33,15 +34,14 @@ public class GetImage : MonoBehaviour
 	private List<Mat> matList=new List<Mat>();	
 
 	[DllImport("__Internal")]  
-	private static extern int testLua(string imgPath, string modelPath);
+	private static extern int testLuaWithArr(float [] arr,int len);
 
-
-	[DllImport("__Internal")]
-	private static extern void _SavePhoto (string readAddr);
+//	[DllImport("__Internal")]
+//	private static extern void _SavePhoto (string readAddr);
 
     void Start() 
 	{
-		rotateCamera = new RotateCamera ();  
+		//rotateCamera = new RotateCamera ();  
 		recognizeAlgo = new RecognizeAlgo();
     }
 
@@ -77,10 +77,8 @@ public class GetImage : MonoBehaviour
             if (webCamTexture.didUpdateThisFrame)
             {
                 frameImg = new Mat(webCamTexture.height, webCamTexture.width, CvType.CV_8UC3);
-
                 texture = new Texture2D(tex_width, tex_height, TextureFormat.RGBA32, false);
                 gameObject.GetComponent<Renderer>().material.mainTexture = texture;
-
                 initDone = true;
                 break;
             }
@@ -96,51 +94,81 @@ public class GetImage : MonoBehaviour
 		TakePhoto();
 	}
 
-
 	public void TakePhoto() 
 	{
-		
         if (!initDone)
             return;
         if (webCamTexture.didUpdateThisFrame)
         {
 			Utils.webCamTextureToMat(webCamTexture, frameImg);
 			Mat tmpImg = frameImg.clone ();
-			rotateCamera.rotate (ref tmpImg);
+			//rotateCamera.rotate (ref tmpImg);
 
 			matList=recognizeAlgo.createDataSet(tmpImg);//切割图片
-			Debug.Log ("matList.Count"+matList.Count);
-
+			//texture.Resize(tmpImg.cols(),tmpImg.rows());
+			Debug.Log ("matList.Count=="+matList.Count);
 			texture.Resize(28, 28);
+			byte[] arr=new byte[28*28*3];
+			float[] sample=new float[arr.Length];
+			if (matList.Count>0) 
+			{
+				for (int i = 0; i < matList.Count; i++) 
+				{
+					Utils.matToTexture2D(matList[i],texture);
+					matList [i].get(0, 0, arr);
+
+				}
+
+				for (int i = 0; i < 100; i++) 
+				{
+					Debug.Log ("arr==="+arr [i]);
+				}	
+				for (int i = 0; i < arr.Length; i++) 
+				{
+					sample [i] = (float)arr [i]/255;
+
+					Debug.Log ("sample==="+sample [i]);
+				}
+				//testLuaWithPara (sample);
+				int ret = testLuaWithArr(sample,sample.Length);
+				Debug.Log ("unity__testLuaWithArr_ret=="+ ret);
+			}
+
 
         }
-		if (matList.Count>0)
-		{
-			for (int i = 0; i < 1/*matList.Count*/; i++) //把小图片存到相册中 
-			{
-				#if UNITY_EDITOR 
-				string path=Application.dataPath+"/Photos/"+System.DateTime.Now.Ticks+".jpg";
-				#elif UNITY_IPHONE 
-				string path =Application.persistentDataPath+"/"+System.DateTime.Now.Ticks+".jpg";
-				Debug.Log ("ios--path===" + path);
-				#endif 
-				Utils.matToTexture2D(matList[i],texture);
-				File.WriteAllBytes(path, texture.EncodeToJPG ());
-
-				string pathIphone = Application.dataPath.Substring (0, Application.dataPath.Length - 4) + "Models/5.mdl";
-				Debug.Log ("path==" + path);
-				Debug.Log ("pathIphone==" + pathIphone);
-				#if UNITY_EDITOR 
-				#elif UNITY_IPHONE  
-
-				_SavePhoto (path);
-				int tempInt=testLua (path, pathIphone);
-				Debug.Log ("tempInt==" + tempInt);
-				#endif 
-
-
-			}
-		}
-
+//		if (matList.Count>0)
+//		{
+//			for (int i = 0; i < 1/*matList.Count*/; i++) //把小图片存到相册中 
+//			{
+//				#if UNITY_EDITOR 
+//				string path=Application.dataPath+"/Photos/"+System.DateTime.Now.Ticks+".jpg";
+//				#elif UNITY_IPHONE 
+//				string path =Application.persistentDataPath+"/"+System.DateTime.Now.Ticks+".jpg";
+//				#endif 
+//
+//				Utils.matToTexture2D(matList[i],texture);
+//				File.WriteAllBytes(path, texture.EncodeToJPG ());
+//
+//				string pathIphone = Application.dataPath.Substring (0, Application.dataPath.Length - 4) + "Models/5.mdl";
+//				Debug.Log ("ios--path===" + path);
+//				Debug.Log ("pathIphone==" + pathIphone);
+//				#if UNITY_EDITOR 
+//				#elif UNITY_IPHONE  
+//				_SavePhoto (path);
+//				testLuaWithPara (path, pathIphone);
+//				#endif 
+//			}
+//		}
     }
+
+
+	void testResult (string msg)
+	{
+		Debug.Log("testResult:" +msg);
+		int ret = int.Parse (msg);
+		Debug.Log ("ret==" + ret);
+	}
+
+
+
 }
